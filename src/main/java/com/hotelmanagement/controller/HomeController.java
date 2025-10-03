ef6c9149c1f9767007275e4da1cf8451bd6b1fa3
Nguyễn Hoàng Phúc <phucnhhe173073@fpt.edu.vn>
2025-10-03 11:59:46 +0700

    Initialize Spring Boot project

--- src/main/java/com/hotelmanagement/controller/HomeController.java
+++ src/main/java/com/hotelmanagement/controller/HomeController.java
@@ -2,11 +2,12 @@ package com.hotelmanagement.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/")
 @Controller
 public class HomeController {
-
-    @GetMapping("/home")
+    @GetMapping
     public String home() {
         return "home";
     }
